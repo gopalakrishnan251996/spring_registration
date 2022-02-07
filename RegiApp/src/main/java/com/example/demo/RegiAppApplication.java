34e7b2d5cd514dcee75e8505bac8f1b5b35a6494
gopalakrishnan251996 <74732417+gopalakrishnan251996@users.noreply.github.com>
2022-02-07 13:07:00 +0530

    Add files via upload

---
+++ RegiApp/src/main/java/com/example/demo/RegiAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegiAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegiAppApplication.class, args);
+	}
+
+}
